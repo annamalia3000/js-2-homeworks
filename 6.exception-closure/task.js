22aaed7c4f47225b262d7d15a864f2036909c9f4
annamalia3000 <a.molch@mail.ru>
2024-02-09 23:34:05 +0300

    Задания с замыканиями

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,52 @@
-﻿
+﻿function parseCount(value) {
+    if (isNaN(Number.parseFloat(value))) {
+        throw new Error("Невалидное значение");
+    } else {
+        return Number.parseFloat(value);
+    }
+}
+
+function validateCount(value) {
+    try { 
+        return parseCount(value);
+    } catch (error) {
+        return error;
+    };
+}
+
+
+class Triangle {
+    constructor(aSide, bSide, cSide) {
+        if (aSide + bSide < cSide || cSide + bSide < aSide || aSide + cSide < bSide) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+        this.aSide = aSide;
+        this.bSide = bSide;
+        this.cSide = cSide;
+    }
+
+    get perimeter() {
+        return (this.aSide + this.bSide + this.cSide);
+    }
+
+    get area() {
+        const halfPerimeter = 0.5 * this.perimeter;
+        const square = Math.sqrt(halfPerimeter * (halfPerimeter - this.aSide) * (halfPerimeter - this.bSide) * (halfPerimeter - this.cSide));
+        return +square.toFixed(3);
+    }
+}
+
+function getTriangle(aSide, bSide, cSide) {
+    try {
+        return new Triangle (aSide, bSide, cSide);
+    } catch (error) {
+        return {
+            get perimeter() {
+                return  "Ошибка! Треугольник не существует";
+            },
+            get area() {
+                return  "Ошибка! Треугольник не существует";
+            }
+        }       
+    }
+}
